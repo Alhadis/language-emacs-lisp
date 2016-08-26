91d5db21650425d3a372dabb339f430e7e0399ec
Alhadis <gardnerjohng@gmail.com>
2016-08-27 02:17:28 +1000

    Avoid matching operators with contiguous characters

--- tests/emacs.el
+++ tests/emacs.el
@@ -2,6 +2,11 @@
       (+ this and that))
 (message (number-to-string (defunct 1 2 3))) ; 6
 
+(funcall (defunct 2 3 4))
+
+(no-funcall (nope))
+(funcall-n (nope))
+
 
 (lambda (x)
   "Return the hyperbolic cosine of X."
@@ -9,12 +14,11 @@
 
 (funcall (lambda (n) (1+ n))        ; One required:
          1)                         ; requires exactly one argument.
-    ; ⇒ 2
+
 (funcall (lambda (n &optional n1)   ; One required and one optional:
            (if n1 (+ n n1) (1+ n))) ; 1 or 2 arguments.
          1 2)
-    ; ⇒ 3
+
 (funcall (lambda (n &rest ns)       ; One required and one rest:
            (+ n (apply '+ ns)))     ; 1 or more arguments.
          1 2 3 4 5)
-    ; ⇒ 15
