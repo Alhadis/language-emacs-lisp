0b32ce1bac06c51c7e47116304bc237574c80a17
Alhadis <gardnerjohng@gmail.com>
2016-08-27 01:25:40 +1000

    Add pattern-matching for bindings and lambdas

--- tests/emacs.el
+++ tests/emacs.el
@@ -1,3 +1,9 @@
 (defun defunct (this and that)
 	(+ this and that))
 (message (number-to-string (defunct 1 2 3))) ; 6
+
+
+
+(lambda (x)
+  "Return the hyperbolic cosine of X."
+  (* 0.5 (+ (exp x) (exp (- x)))))
